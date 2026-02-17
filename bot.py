0c42f042e762bdffb278c79f966ae955939250a6
1emadrza13881-max <1emadrza13881@gmail.com>
2026-02-18 00:43:48 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.environ.get("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ربات روشن شد 🚀")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("دستور /start رو بزن 😉")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
+
+print("Bot is running...")
+app.run_polling()
